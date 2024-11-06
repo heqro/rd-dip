c4bc9755208941ffe94a9a56c9d672ccb8884b8d
Rodrigo Iglesias <hr.iglesias.2018@alumnos.urjc.es>
2024-11-06 14:46:19 +0000

    🖥️ Upload script for using our own images with RicianNet

--- .riciannet/RicianNet/.my_own_testing/test_my_images.py
+++ .riciannet/RicianNet/.my_own_testing/test_my_images.py
@@ -6,29 +6,87 @@ from skimage.metrics import peak_signal_noise_ratio as psnr
 from PIL import Image
 
 
-u = np.array(Image.open('Original_Mine.png').convert('L')) 
-u = u /u.max()
-# plt.imsave('Original_Mine.png', u, cmap='gray')
+def pad_image(img: np.ndarray, new_height: int, new_width: int):
 
-noise_factor = .15
-sigma=noise_factor * u.max()
+    padding = np.zeros((new_height, new_width))
+    img_height, img_width = img.shape
+    padding[:img_height, :img_width] = img
+    return padding
 
-f_real = u + sigma * np.random.randn(u.shape[0], u.shape[1])
-f_imag = sigma * np.random.randn(u.shape[0], u.shape[1])
+
+def psnr_with_mask(
+    img_1: np.ndarray,
+    img_2: np.ndarray,
+    mask: np.ndarray,
+    data_range=1.0,
+):
+    mask_size = (mask > 0).sum().item()
+    mse = ((img_1 - img_2) ** 2 * mask).sum() / mask_size
+    return 10 * np.log10(data_range**2 / mse)
+
+
+mask = np.array(
+    Image.open(
+        "/mnt/data_drive/hrodrigo/mri_rician_noise/.nppy_testing/images_exported_to_png/scalar_field.png"
+    ).convert("L")
+)
+mask = mask / mask.max()
+gt = np.array(
+    Image.open(
+        "/mnt/data_drive/hrodrigo/mri_rician_noise/.nppy_testing/images_exported_to_png/slice_original.png"
+    ).convert("L")
+)
+gt = gt / gt.max()
+
+noise_factor = 0.15
+sigma = noise_factor * gt.max()
+
+gt = pad_image(gt, 256, 270)
+mask = pad_image(mask, 256, 270)
+f_real = gt + sigma * np.random.randn(gt.shape[-2], gt.shape[-1])
+f_imag = sigma * np.random.randn(gt.shape[-2], gt.shape[-1])
 f = np.sqrt(f_real**2 + f_imag**2)
-residue = f-u
+residue = f - gt
 
-plt.imsave(f'Contamined_{noise_factor}_Mine.png', f, cmap='gray')
+plt.imsave(
+    f"/mnt/data_drive/hrodrigo/mri_rician_noise/.riciannet/RicianNet/.my_own_testing/Contamined_{noise_factor}_Mine.png",
+    f,
+    cmap="gray",
+)
 
-plt.imsave(f'Residue_{noise_factor}_Mine.png', residue, cmap='gray')
 
-prototext_path = '/mnt/data_drive/hrodrigo/mri_rician_noise/.riciannet/RicianNet/Riciannet_deploy.prototxt'
-weights_path = '/mnt/data_drive/hrodrigo/mri_rician_noise/.riciannet/RicianNet/model/N15_complex_Brain1&2.caffemodel'
+prototext_path = "/mnt/data_drive/hrodrigo/mri_rician_noise/.riciannet/RicianNet/Riciannet_deploy.prototxt"
+weights_path = "/mnt/data_drive/hrodrigo/mri_rician_noise/.riciannet/RicianNet/model/N15_complex_Brain1&2.caffemodel"
 
-caffe.set_mode_cpu() 
+caffe.set_mode_cpu()
 net = caffe.Net(prototext_path, weights_path, caffe.TEST)
 
 result = net.forward(data=f.reshape(1, 1, 256, 270))
-result_conv13=result['conv13'].reshape(256,270)
-plt.imsave(f'Denoised_{noise_factor}_Mine.png', result_conv13, cmap='gray')
+result_conv13 = result["conv13"].squeeze()
 
+
+result_conv13_normalized = (result_conv13 - result_conv13.min()) / (
+    result_conv13.max() - result_conv13.min()
+)
+
+print(f"PSNR is {psnr(gt, result_conv13)}")  # 27.76
+print(f"PSNR is {psnr_with_mask(result_conv13, gt, mask)}")  # 24.14
+
+
+plt.imsave(
+    f"/mnt/data_drive/hrodrigo/mri_rician_noise/.riciannet/RicianNet/.my_own_testing/Denoised_{noise_factor}_RicianNet.png",
+    result_conv13,
+    cmap="gray",
+)
+
+plt.imsave(
+    f"/mnt/data_drive/hrodrigo/mri_rician_noise/.riciannet/RicianNet/.my_own_testing/denoised_RicianNet_masked.png",
+    (result_conv13 * mask),
+    cmap="gray",
+)
+
+# plt.imsave(
+#     f"/mnt/data_drive/hrodrigo/mri_rician_noise/.riciannet/RicianNet/.my_own_testing/denoised_RicianNet_masked_normalized.png",
+#     (result_conv13_normalized * mask),
+#     cmap="gray",
+# )
